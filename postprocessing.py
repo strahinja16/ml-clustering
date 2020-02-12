266826f9891ffc5333376e674cd6c2273a285898
Uros <danijelauros@gmail.com>
2020-02-12 01:01:30 +0100

    polishing main.ipynb for demonstation

--- postprocessing.py
+++ postprocessing.py
@@ -2,7 +2,7 @@ import pandas as pd
 import json
 from os import path, remove
 
-PROCESSED_FILE_NAME = './clustered_data.csv'
+PROCESSED_FILE_NAME = './processed_data_with_cluster.csv'
 POST_PROCESSED_FILE_NAME = './post_processed_data.csv'
 JSON_FILE_NAME = './dictionaries.json'
 
@@ -15,7 +15,7 @@ TYPE_MAP = {
 
 processed_data = pd.read_csv(PROCESSED_FILE_NAME)
 data_columns = [
-    # 'event_time',
+    'event_time',
     'event_type',
     'product_id',
     'category_id',
@@ -60,7 +60,7 @@ for index, row in processed_data.iterrows():
         'event_type': TYPE_MAP[row["event_type"]],
         'product_id': get_key(row["product_id"], new_dict["product_map"]),
         'category_id': get_key(row["category_id"], new_dict["category_map"]),
-        'category_code': get_key(row["category_id"], new_dict["category_map"]),
+        # 'category_code': get_key(row["category_id"], new_dict["category_map"]),
         'brand': get_key(row["brand"], new_dict["brand_map"]),
         'price': row["price"],
         'user_id': get_key(row["user_id"], new_dict["user_map"]),
